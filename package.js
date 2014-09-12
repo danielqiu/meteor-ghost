8aa930ff5703b2585b4aff088a447790290347f9
Daniel Qiu <daniel@danielqiu.com>
2014-09-11 20:55:46 -0700

    .

--- package.js
+++ package.js
@@ -6,7 +6,7 @@ Package.describe({
 	git: "https://github.com/danielqiu/meteor-ghost.git"
 });
 
-Npm.depends({ghost: "0.5"});
+Npm.depends({ghost: "0.5.0"});
 
 Package.on_use(function(api){
 	api.versionsFrom('METEOR@0.9.1.1');
