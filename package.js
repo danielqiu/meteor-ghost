5407b29f298be3f60d59e6d0e479e91916a8fdbf
Daniel Qiu <daniel@danielqiu.com>
2014-09-11 20:37:08 -0700

    .

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-	name: "ghost",
+	name: "danielqiu:ghost",
 	summary: "Ghost blogging platform in meteor",
 	author: "Daniel Qiu",
 	version: "0.4.3",
@@ -9,6 +9,7 @@ Package.describe({
 Npm.depends({ghost: "0.4.2"});
 
 Package.on_use(function(api){
+	api.versionsFrom('METEOR-CORE@0.9.0');
 	api.add_files(['ghost.js'], 'server', 'client');
 	if(api.export)
 		api.export('ghost');
