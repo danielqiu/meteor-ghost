62a6c1974bbb6ca087fd6e9e2fd5bfb82a0e63d1
Daniel Qiu <daniel@danielqiu.com>
2015-03-20 09:37:40 -0700

    .

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
 	name: "mrt:ghost",
 	summary: "Ghost blogging platform in meteor",
 	author: "Daniel Qiu",
-	version: "0.5.2",
+	version: "0.5.2.1",
 	git: "https://github.com/danielqiu/meteor-ghost.git"
 });
 
