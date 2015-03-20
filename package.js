6ba1c0f03bd9c6b47963eb8d1de4694136b94563
Daniel Qiu <daniel@danielqiu.com>
2015-03-20 00:08:02 -0700

    bumps the version to 0.5.2

--- package.js
+++ package.js
@@ -2,15 +2,15 @@ Package.describe({
 	name: "mrt:ghost",
 	summary: "Ghost blogging platform in meteor",
 	author: "Daniel Qiu",
-	version: "0.5.0",
+	version: "0.5.2",
 	git: "https://github.com/danielqiu/meteor-ghost.git"
 });
 
-Npm.depends({ghost: "0.5.0"});
-
 Package.on_use(function(api){
-	api.versionsFrom('METEOR@0.9.1.1');
+	api.versionsFrom('1.0');
 	api.add_files(['ghost.js'], 'server', 'client');
 	if(api.export)
 		api.export('ghost');
 });
+
+Npm.depends({ghost: "0.5.2"});
